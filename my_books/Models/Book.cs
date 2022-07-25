03fd00e0c553a9dda13dee8fffc75c821df09585
PriyaIk <107747801+PriyaIk@users.noreply.github.com>
2022-07-25 21:29:24 +0530

    second commit

--- my_books/Models/Book.cs
+++ my_books/Models/Book.cs
@@ -2,5 +2,11 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
     }
 }
